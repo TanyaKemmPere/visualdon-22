406331e2573bf75fbf5e871b37634a9ccc09c7f5
Audreycks <72491693+audreycks@users.noreply.github.com>
2022-04-01 11:13:44 +0200

    importation des données

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,7 +1,10 @@
 import * as d3 from 'd3'
+import pib from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv';
+import esperance from '../data/life_expectancy_years.csv';
+import population from '../data/population_total.csv';
 
-// Pour importer les données
-// import file from '../data/data.csv'
+// Le premier rendu implique la visualisation statique des données data/gapminder.csv pour l'année 2021 sous forme de Scatter/Bubble Chart. 
+//Vous aurez sur l'axe X les données de PIB par habitant et sur l'axe Y l'espérance de vie. La taille des cercles devra être proportionnelle à la population du pays.
 
 
 
