4f57d39dda5c1b8c61260b2b9fcf6d35e8c7a2ba
Audreycks <72491693+audreycks@users.noreply.github.com>
2022-04-02 13:20:37 +0200

    dessin cercle + essai de résolution pour pib2021

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -8,9 +8,27 @@ import population from "../data/population_total.csv";
 //La taille des cercles devra être proportionnelle à la population du pays.
 
 const pib2021 = pib.map((d) => {
-  return d[2021];
+  return parseInt(d[2021]);
 });
-const maxpib2021 = d3.max(pib2021); //ici ce n'est pas la valeur max qui est prise… (pourquoi?)
+const maxpib2021 = Math.max(...pib2021); //ici ce n'est pas la valeur max qui est prise… (pourquoi?)
+
+//il faudrait pouvoir prendre la lettre suivant le chiffre du tableau et ajouter les 0 qu'il faut
+
+/*
+if(k){
+    … * 10^3
+}
+elseif(M){
+    … * 10^6
+}
+elseif(B){
+    … * 10^9
+}
+else {
+    return pib2021
+}
+
+*/
 
 const esperance2021 = esperance.map((d) => {
   return d[2021];
@@ -21,9 +39,9 @@ const population2021 = population.map((d) => {
   return d[2021];
 });
 
-console.log(pib2021);
-console.log(esperance2021);
-console.log(population2021);
+// console.log(pib2021);
+// console.log(esperance2021);
+// console.log(population2021);
 
 const svg = d3
   .select("body")
@@ -32,8 +50,8 @@ const svg = d3
   .attr("height", width);
 
 const margin = { top: 10, right: 40, bottom: 20, left: 40 },
-width = window.innerWidth*0.8,
-height = window.innerHeight*0.8;
+  width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8;
 
 svg
   .attr("width", width + margin.left + margin.right)
@@ -52,13 +70,25 @@ svg
   .call(d3.axisBottom(x));
 
 // axe Y
-const y = d3
-  .scaleLinear()
-  .domain([0, maxesperance2021])
-  .range([height, 0]); 
+const y = d3.scaleLinear().domain([0, maxesperance2021]).range([height, 0]);
 
 svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
   .call(d3.axisLeft(y));
+
+//il faut rassembler toutes les données en un tableau pour pouvoir dessiner les cercles
+
+/*  let data = [];
+  data.push(population2021, pib2021, esperance2021); */
+
+svg
+  .select("body")
+  .append("circle")
+  .data(population2021, pib2021, esperance2021)
+  .enter()
+  .attr("r", (d) => d.population2021)
+  .attr("cx", (d) => x(d.pib2021))
+  .attr("cy", (d) => y(d.esperance2021))
+  .attr("fill", "pink");
 
