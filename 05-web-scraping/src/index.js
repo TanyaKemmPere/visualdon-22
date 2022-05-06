f62306b7dcaf9402643d36c7e8dae7995bbd6731
Audreycks <72491693+audreycks@users.noreply.github.com>
2022-05-06 09:49:23 +0200

    puppeteer fonctionne 🎉

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -2,14 +2,24 @@ import jsdom from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
-//ne fonctionne pas
-//const puppeteer = require('puppeteer');
+
+/* //const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Données_cantonales');
-  await page.screenshot({ path: './05-web-scraping/src/cantons.png' });
+  await page.screenshot({ path: 'cantons.png' });
 
   await browser.close();
-})();
+})(); */
+
+const getScreenshot = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Données_cantonales')
+  await page.screenshot({ path: 'cantons.png' })
+  await browser.close()
+}
+
+getScreenshot()
