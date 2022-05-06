632af380f42f4c8a345dee01e45ecb39c76c519c
Audreycks <72491693+audreycks@users.noreply.github.com>
2022-05-06 09:33:10 +0200

    puppeteer installation + mise en place
    ne fonctionne pas

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -2,3 +2,14 @@ import jsdom from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
+//ne fonctionne pas
+//const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Données_cantonales');
+  await page.screenshot({ path: './05-web-scraping/src/cantons.png' });
+
+  await browser.close();
+})();
